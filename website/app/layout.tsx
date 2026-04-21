08298f4da7e48ba8ad923b50745198282e0b55b3
Hikaru Kigen <hikarunokigen@gmail.com>
2026-04-21 02:44:53 -0700

    project: rename project as "detective"

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from "react";
 import "./globals.scss";
 
 export const metadata = {
-  title: "detective_k",
+  title: "detective",
   description: "ygosu sleuthing",
 };
 
